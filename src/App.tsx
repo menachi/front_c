819b2ba988eea75165eb36a7ebb8194b70d33ee6
Eliav Menachi <eliav.menachi@gmail.com>
2025-01-01 16:53:45 +0200

    enc class 2 react

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,83 @@
 import PostsList from "./PostsList"
 
-
 function App() {
+
   const name = 'Eliav'
   const getName = () => {
     return name
   }
+  console.log('App')
+  const items = ['item1', 'item2', 'item3']
+
+  const onItemsSelected = (index: number) => {
+    console.log('App - Selected index is', index)
+
+  }
+
   return (
     <>
-      <div>
+      <div style={{
+        backgroundColor: 'grey',
+        margin: '10px',
+        padding: '10px',
+        borderRadius: '10px',
+        width: '500px'
+      }}>
         <h1>Hello, React {getName()}</h1>
-        <PostsList />
+        <PostsList title="this is my array" items={items} onItemsSelected={onItemsSelected} />
       </div>
+      <div style={{
+        backgroundColor: 'grey',
+        margin: '10px',
+        padding: '10px',
+        borderRadius: '10px',
+        width: '500px'
+      }}>
+        <h1>Hello, React {getName()}</h1>
+        <PostsList title="this is my array" items={items} onItemsSelected={onItemsSelected} />
+      </div>
+
     </>
 
   )
 }
 
-export default App
+function App2() {
+  return (
+    <div style={{
+      width: '100vw',
+      height: '500px',
+      display: 'flex',
+      flexDirection: 'row-reverse',
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <div style={{
+        backgroundColor: 'red',
+        width: '100px',
+        height: '100px'
+      }}>
+      </div>
+      <div style={{
+        backgroundColor: 'blue',
+        width: '100px',
+        height: '100px',
+        flex: 1,
+        position: 'absolute',
+        top: '50px',
+        right: '150px'
+
+      }}>
+      </div>
+      <div style={{
+        backgroundColor: 'yellow',
+        width: '100px',
+        height: '100%',
+        flex: 1
+      }}>
+      </div>
+    </div>
+  )
+}
+
+export default App2
--- src/PostsList.tsx
+++ src/PostsList.tsx
@@ -1,13 +1,56 @@
+import { useState, FC } from 'react';
 
 
-function PostsList() {
+interface PostsListProps {
+    title: string,
+    items: string[],
+    onItemsSelected: (index: number) => void
+}
+const PostsList: FC<PostsListProps> = ({ title, items, onItemsSelected }) => {
     console.log('PostsList');
-    const items = ['An item', 'A second item', 'A third item', 'A fourth item', 'And a fifth one'];
+    const [selectedIndex, setSelectedIndex] = useState(0)
+    const [render, setRender] = useState(0);
+
+    const onClick = (index: number) => {
+        console.log('Clicked on', index);
+        setSelectedIndex(index);
+    }
+
+    const addItem = () => {
+        console.log('Adding an item');
+        items.push('A new item');
+        setRender(render + 1);
+
+    }
+
+    const onSelect = () => {
+        console.log('Selected index is', selectedIndex);
+        onItemsSelected(selectedIndex);
+    }
     return (
-        <ul className="list-group">
-            {items.map((item, index) => { return <li className="list-group-item" key={index}>{index}: {item}</li> })}
-        </ul>
+        <>
+            <h1>{title}</h1>
+            <ul className="list-group">
+                {
+                    items.map((item, index) => {
+                        return <li
+                            className={index == selectedIndex ? "list-group-item active" : "list-group-item"}
+                            key={index}
+                            onClick={() => { onClick(index) }}
+                        >
+                            {index}: {item}
+                        </li>
+                    })
+                }
+            </ul >
+            <button className="btn btn-primary m-3" onClick={addItem}>Add</button>
+            <button className="btn btn-primary" onClick={onSelect}>Selected</button>
+
+        </>
     );
+
 }
 
 export default PostsList;
+
+
